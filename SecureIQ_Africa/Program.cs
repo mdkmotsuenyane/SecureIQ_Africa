7fa3274774394874cc25236118131b89986d8e5f
Student <Student@LabVM2049939>
2026-04-09 11:18:02 +0200

    Add project files.

---
+++ SecureIQ_Africa/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using static System.Console;
+
+namespace SecureIQ_Africa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //declaration
+            string name, message;
+
+            message = " Welcome to SecureIQ Africa, your trusted partner in cybersecurity awareness. How can we help you today?";
+
+
+            //prints logo
+            WriteLine("  █████████                                                  █████    ██████         █████████      ██████             ███                    \r\n ███░░░░░███                                                ░░███   ███░░░░███      ███░░░░░███    ███░░███           ░░░                     \r\n░███    ░░░   ██████   ██████  █████ ████ ████████   ██████  ░███  ███    ░░███    ░███    ░███   ░███ ░░░  ████████  ████   ██████   ██████  \r\n░░█████████  ███░░███ ███░░███░░███ ░███ ░░███░░███ ███░░███ ░███ ░███     ░███    ░███████████  ███████   ░░███░░███░░███  ███░░███ ░░░░░███ \r\n ░░░░░░░░███░███████ ░███ ░░░  ░███ ░███  ░███ ░░░ ░███████  ░███ ░███   ██░███    ░███░░░░░███ ░░░███░     ░███ ░░░  ░███ ░███ ░░░   ███████ \r\n ███    ░███░███░░░  ░███  ███ ░███ ░███  ░███     ░███░░░   ░███ ░░███ ░░████     ░███    ░███   ░███      ░███      ░███ ░███  ███ ███░░███ \r\n░░█████████ ░░██████ ░░██████  ░░████████ █████    ░░██████  █████ ░░░██████░██    █████   █████  █████     █████     █████░░██████ ░░████████\r\n ░░░░░░░░░   ░░░░░░   ░░░░░░    ░░░░░░░░ ░░░░░      ░░░░░░  ░░░░░    ░░░░░░ ░░    ░░░░░   ░░░░░  ░░░░░     ░░░░░     ░░░░░  ░░░░░░   ░░░░░░░░ \r\n                                                                                                                                              \r\n                                                                                                                                              \r\n                                                                                                                                              ");
+            WriteLine("                       .,,uod8B8bou,,.\r\n              ..,uod8BBBBBBBBBBBBBBBBRPFT?l!i:.\r\n         ,=m8BBBBBBBBBBBBBBBRPFT?!||||||||||||||\r\n         !...:!TVBBBRPFT||||||||||!!^^\"\"'   ||||\r\n         !.......:!?|||||!!^^\"\"'            ||||\r\n         !.........||||                     ||||\r\n         !.........||||  ##                 ||||\r\n         !.........||||                     ||||\r\n         !.........||||                     ||||\r\n         !.........||||                     ||||\r\n         !.........||||                     ||||\r\n         `.........||||                    ,||||\r\n          .;.......||||               _.-!!|||||\r\n   .,uodWBBBBb.....||||       _.-!!|||||||||!:'\r\n!YBBBBBBBBBBBBBBb..!|||:..-!!|||||||!iof68BBBBBb....\r\n!..YBBBBBBBBBBBBBBb!!||||||||!iof68BBBBBBRPFT?!::   `.\r\n!....YBBBBBBBBBBBBBBbaaitf68BBBBBBRPFT?!:::::::::     `.\r\n!......YBBBBBBBBBBBBBBBBBBBRPFT?!::::::;:!^\"`;:::       `.\r\n!........YBBBBBBBBBBRPFT?!::::::::::^''...::::::;         iBBbo.\r\n`..........YBRPFT?!::::::::::::::::::::::::;iof68bo.      WBBBBbo.\r\n  `..........:::::::::::::::::::::::;iof688888888888b.     `YBBBP^'\r\n    `........::::::::::::::::;iof688888888888888888888b.     `\r\n      `......:::::::::;iof688888888888888888888888888888b.\r\n        `....:::;iof688888888888888888888888888888888899fT!\r\n          `..::!8888888888888888888888888888888899fT|!^\"'\r\n            `' !!988888888888888888888888899fT|!^\"'\r\n                `!!8888888888888888899fT|!^\"'\r\n                  `!988888888899fT|!^\"'\r\n                    `!9899fT|!^\"'\r\n                      `!^\"'\r\n");
+            //prompt the user 
+            Write("What is your name : ");
+            name = ReadLine();
+
+            //System voice
+            SpeechSynthesizer voice = new SpeechSynthesizer();
+            voice.Speak(message);
+
+            //Creates a new instance for the response class
+            Response respond = new Response();
+            respond.name = name;
+
+            WriteLine(message + " " + name);
+
+
+        }
+        public string Validate(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                WriteLine("Please enter a valid question.");
+                // Return null for invalid input
+                return null;
+            }
+
+            if (input.Equals("exit", StringComparison.OrdinalIgnoreCase))
+            {
+                WriteLine("Goodbye! Stay safe online.");
+                // closes the program
+                Environment.Exit(0);
+            }
+            // Return the valid input
+            return input;
+        }
+
+
+
+    }
+}
---
+++ SecureIQ_Africa/Response.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace SecureIQ_Africa
+{
+    internal class Response
+    {
+        //get and set name
+        public string name { get; set; }
+
+
+        //dictionary is used to store all the posssible questions
+        private Dictionary<string, (string[] keywords, string response)> secureData =
+            new Dictionary<string, (string[], string)>()
+        {
+            {
+                "password",
+                (new[] { "password", "pass", "login", "credential" },
+                "Avoid using personal information and use strong passwords with uppercase, lowercase, numbers, and symbols.")
+            },
+            {
+                "phishing",
+                (new[] { "phishing", "scam", "fake email", "suspicious link" },
+                "Do not click suspicious links. Always verify the sender before responding.")
+            },
+            {
+                "malware",
+                (new[] { "malware", "virus", "trojan", "spyware" },
+                "Install antivirus software and avoid downloading from untrusted sources.")
+            },
+            {
+                "wifi",
+                (new[] { "wifi", "public wifi", "network", "hotspot" },
+                "Avoid sensitive transactions on public Wi-Fi. Use a VPN if possible.")
+            },
+            {
+                "greeting",
+                (new[] { "hello", "hi", "hey" },
+                "Hello! I'm your cybersecurity awareness bot.")
+            },
+            {
+                "help",
+                (new[] { "help", "what can i ask", "what do you know" },
+                "You can ask me about passwords, phishing, malware, safe browsing, and more.")
+            }
+        };
+
+        //used to get response
+        public string GetResponse(string userInput)
+        {
+            if (string.IsNullOrWhiteSpace(userInput))
+                return "Please ask me something about cybersecurity!";
+
+            userInput = userInput.ToLower();
+
+            foreach (var item in secureData)
+            {
+                foreach (var keyword in item.Value.keywords)
+                {
+                    if (userInput.Contains(keyword))
+                    {
+                        return item.Value.response;
+                    }
+                }
+            }
+
+            return "Sorry, I don't understand that yet.";
+        }
+
+        //  prints response
+        public void Ask(string question)
+        {
+            string response;
+            response = GetResponse(question);
+            WriteLine(name + " " + response);
+        }
+
+
+
+        public void Welcome()
+        {
+            string message = " Welcome to SecureIQ Africa, your trusted partner in cybersecurity awareness. How can we help you today : ";
+            WriteLine(" █████   ███   █████          ████                                            \r\n░░███   ░███  ░░███          ░░███                                            \r\n ░███   ░███   ░███   ██████  ░███   ██████   ██████  █████████████    ██████ \r\n ░███   ░███   ░███  ███░░███ ░███  ███░░███ ███░░███░░███░░███░░███  ███░░███\r\n ░░███  █████  ███  ░███████  ░███ ░███ ░░░ ░███ ░███ ░███ ░███ ░███ ░███████ \r\n  ░░░█████░█████░   ░███░░░   ░███ ░███  ███░███ ░███ ░███ ░███ ░███ ░███░░░  \r\n    ░░███ ░░███     ░░██████  █████░░██████ ░░██████  █████░███ █████░░██████ \r\n     ░░░   ░░░       ░░░░░░  ░░░░░  ░░░░░░   ░░░░░░  ░░░░░ ░░░ ░░░░░  ░░░░░░  \r\n                                                                              \r\n                                                                              \r\n                                                                              ");
+            WriteLine(message + " " + name);
+        }
+
+    }
+}
