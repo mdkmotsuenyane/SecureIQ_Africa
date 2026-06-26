def126cb41972416ae4cf6d3f4d1c8efcf344e2a
Kholo1511 <khololuhle.motsuenyane@gmail.com>
2026-06-26 11:55:42 +0200

    Add files via upload
    uploaded the database

---
+++ TaskChat/Task.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Task]
+(
+	[Id] INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+	[Title] VARCHAR(50) NOT NULL,
+	[Description] VARCHAR(255) NOT NULL,
+	[Remainder] VARCHAR(50) NULL,
+	[IsCompleted] BIT NOT NULL DEFAULT 0
+)
